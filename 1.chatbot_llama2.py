bb6838f8679ed77e5d4367b666877f388a57da9e
aiXpertLab <aiXpertLab@gmail.com>
2024-03-08 20:39:59 -0500

    basic model

---
+++ 1.chatbot_llama2.py
@@ -0,0 +1,32 @@
+from langchain_community.llms       import LlamaCpp, CTransformers
+from langchain_community.embeddings import LlamaCppEmbeddings, HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma, FAISS
+from langchain_community.document_loaders import DirectoryLoader, TextLoader, PyPDFLoader, WebBaseLoader, PyPDFDirectoryLoader
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, RetrievalQA
+
+# Load the LlamaCpp language model, adjust GPU usage based on your hardware
+llm = LlamaCpp(
+    model_path = "e:/models/llama/llama-2-7b-chat.Q6_K.gguf",
+    n_gpu_layers=40,
+    n_batch=512,  # Batch size for model processing
+)
+
+# Define the prompt template with a placeholder for the question
+template = """
+Question: {question}
+
+Answer:
+"""
+prompt = PromptTemplate(template=template, input_variables=["question"])
+
+# Create an LLMChain to manage interactions with the prompt and model
+llm_chain = LLMChain(prompt=prompt, llm=llm)
+
+print("Chatbot initialized, ready to chat...")
+while True:
+    question = input("> ")
+    answer = llm_chain.run(question)
+    print(answer, '\n')
